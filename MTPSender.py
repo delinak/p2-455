d99b82f5ff3575631c112e055c772b85a084073b
delinak <77298609+delinak@users.noreply.github.com>
2024-03-30 16:00:49 -0400

    minor updates

--- MTPSender.py
+++ MTPSender.py
@@ -18,22 +18,22 @@ class PacketSender:
         self.lock = threading.Lock()
 
          # opening log file to start logging
-        self.fLog = open(log_file,'a')
+        self.filelogging = open(log_file,'a')
 
         #dict for header and data
-        self.windowPkt = {}
+        self.window_pkt = {}
         self.received_pkts = []
         self.sent = 0
 
-    def initialize_windowList(tot_packets):
+    def initialize_windowlist(self, tot_packets):
         for pkt in tot_packets:
             self.windowPkt[pkt] = 1
 
-    def print_windowList(self):
-        self.fLog.write("Window state:[")
+    def print_windowlist(self, tot_packets):
+        self.filelogging.write("Window state:[")
         for pkt in tot_packets:
-            self.fLog.write(f"%d(%d),", pkt, self.windowPkt[pkt])
-        self.fLog.write("]\n")
+            self.filelogging.write("%d(%d),", pkt, self.window_pkt[pkt])
+        self.filelogging.write("]\n")
 
     # breaks up the file into chunks
     def create_packet(self):
@@ -51,58 +51,63 @@ class PacketSender:
                 encoded_data = data.encode('utf-8')
                 checksum = zlib.crc32(encoded_data)
 
-                headerData = struct.pack('!IIII', 1, self.seq_number, length, checksum, data)
-                packet[self.seq_number] = headerData
+                headerdata = struct.pack('!IIII', 1, self.seq_number, length, checksum, data)
+                packet[self.seq_number] = headerdata
     
         return packet
+    
+    
+    def ack_received_packet(self, ty, seq, length, checksum_in_packet, left_window, right_window, tot_packets):
+        checksum_calculated = zlib.crc32(seq,length)
+        if (checksum_in_packet == checksum_calculated):
+
+            right_window = min(right_window + 1, len(self.packet)-1)
+            if right_window - left_window >= window_size: # maintain window size
+                left_window += 1
+            self.received_pkts = None # reset received packets list
+
+            self.lock.acquire()
+            self.filelogging.write("Updating window;(show seqNum of %d packets in the window with one bit status (0: sent but not acked, 1:not sent)\n", tot_packets)
+            self.print_windowlist(tot_packets)
+            self.filelogging.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}""\n")
+            self.lock.release()
+
+            flag = 0 # acked packet
+            return left_window, right_window, flag
+        else: # ignore corrupt files
+            self.lock.acquire()
+            self.filelogging.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}; checksum_calculated={checksum_calculated}; status=CORRUPT; \n")
+            self.lock.release()
+            return left_window, right_window, 0 # flag = 0, no resending
+        
     
     def extract_packet(self, left_window, right_window, tot_packets):
         if self.received_pkts: # if false, then timeout so resend packet
             for ack in self.received_pkts:
                 ty, seq, length, checksum_in_packet = struct.unpack('!IIII', ack)
                 if left_window == seq:
-                    checksum_calculated = zlib.crc32(seq,length,ty)
-                    if (checksum_in_packet == checksum_calculated):
-
-                        right_window = min(right_window + 1, len(self.packet)-1)
-                        if right_window - left_window >= window_size: # maintain window size
-                            left_window += 1
-                        self.received_pkts = None # reset received packets list
-
-                        self.lock.acquire()
-                        self.fLog.write("Updating window;(show seqNum of %d packets in the window with one bit status (0: sent but not acked, 1:not sent)\n", tot_packets)
-                        self.print_windowState()
-                        self.fLog.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}""\n")
-                        self.lock.release()
-
-                        flag = 0 # acked packet
-                        return left_window, right_window, flag
-                    else: # ignore corrupt files
-                        self.lock.acquire()
-                        self.fLog.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}; checksum_calculated={checksum_calculated}; status=CORRUPT\n")
-                        self.lock.release()
-                        return left_window, right_window, 0 # flag = 0, no resending
+                   self.ack_received_packet(ty, seq, length, checksum_in_packet, left_window, right_window, tot_packets)
                 else: # check for triple acks
                     if seq in self.seen_acks:
                         self.seen_acks[seq] += 1
                         if self.seq_acks[seq] >= 3:
                             self.lock.acquire()
-                            self.fLog.write("Triple dup acks received for packet seqNum=%d", seq)
+                            self.filelogging.write("Triple dup acks received for packet seqNum=%d", seq)
                             self.lock.release()
                     else:
                         self.seen_acks[seq] = 1
         else:
             self.lock.acquire()
-            self.fLog.write("Timeout for packet seqNum=%d",seq)
+            self.filelogging.write("Timeout for packet seqNum=%d",left_window)
             self.lock.release()
 
         self.received_pkts = None
         flag = 0 # reset received packets list
         return left_window, right_window, flag
 
-    def receive_thread(self, UDPClientSocket):
+    def receive_thread(self, clientsocket):
         while True:
-            packet_from_server, server_address = unreliable_channel.recv_packet(UDPClientSocket)
+            packet_from_server, _ = unreliable_channel.recv_packet(clientsocket)
             self.received_pkts.append(packet_from_server)
 
     def main(self):
@@ -110,11 +115,11 @@ class PacketSender:
         packet = processor.create_packet()
 
         # open client socket and bind
-        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        UDPClientSocket.bind((ip_address, port_number))
+        clientsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        clientsocket.bind((ip_address, port_number))
         
         # start receive thread
-        recv_thread = threading.Thread(target=self.receive_thread,args=(UDPClientSocket,))
+        recv_thread = threading.Thread(target=self.receive_thread,args=(clientsocket,))
         
         tot_packets = len(packet)
         self.initialize_windowListener(tot_packets) 
@@ -126,11 +131,11 @@ class PacketSender:
             recv_thread.start()
             while time.time() - start_time < 500/1000:
                 while curr_pkt <= right_window:
-                    unreliable_channel.send_packet(UDPClientSocket, packet[curr_pkt], ip_address) # add receiver addy
-                    self.windowPkt[curr_pkt] = 0
+                    unreliable_channel.send_packet(clientsocket, packet[curr_pkt], ip_address) # add receiver addy
+                    self.window_pkt[curr_pkt] = 0
                     ty, seq, length, checksum = struct.unpack('!IIII',  packet[curr_pkt])
                     self.lock.aquire()
-                    self.fLog.write(f"Packet sent; type={ty}; seqNum={seq}; length={length}; checksum={checksum}""\n")
+                    self.filelogging.write(f"Packet sent; type={ty}; seqNum={seq}; length={length}; checksum={checksum}""\n")
                     self.lock.release()
                     curr_pkt += 1
                 
@@ -138,7 +143,7 @@ class PacketSender:
 
             if flag == 0: # retransmit last unacked packet
                 start_time = time.time()
-                unreliable_channel.send_packet(UDPClientSocket, packet[left], ip_address) # add receiver addy
+                unreliable_channel.send_packet(clientsocket, packet[left], ip_address) # add receiver addy
                 while time.time() - start_time < 500/1000: # wait for packet to be received
                     self.extract_packet(left_window, right_window, tot_packets)
 
@@ -147,7 +152,7 @@ class PacketSender:
             right_window = right
             self.sent += 1
         
-        self.fLog.close()
+        self.filelogging.close()
 
 if __name__ == "__main__":
     # read data from command line args
