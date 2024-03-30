5faf17fc8a9b62a7da332c366c20a99427b8929a
delinak <77298609+delinak@users.noreply.github.com>
2024-03-30 14:23:44 -0400

    added logging

--- MTPSender.py
+++ MTPSender.py
@@ -15,8 +15,11 @@ class PacketSender:
         self.input_file = input_file
         self.log_file = log_file
         self.seq_number = seq_number
-
         self.lock = threading.Lock()
+
+         # opening log file to start logging
+        self.fLog = open(log_file,'a')
+
         #dict for header and data
         self.windowPkt = {}
         self.received_pkts = []
@@ -41,29 +44,49 @@ class PacketSender:
                 packet[self.seq_number] = headerData
     
         return packet
+    
+    def retransmit_oldest_packet(self):
+        # no packets received due to timeout so resend
+        start_time = time.time()
+        while time.time() - start_time < 500/1000:
+            unreliable_channel.send_packet(UDPClientSocket, packet[curr], ip_address) # add receiver addy
+            self.extract_packet(left_window, right_window, curr_pkt)
 
     def extract_packet(self, left_window, right_window, curr):
         # extract the packet data after receiving
         if self.received_pkts: # if false, then timeout so resend packet
             for ack in self.received_pkts:
-                ty, seq, length, checksum = struct.unpack('!IIII', ack)
+                ty, seq, length, checksum_in_packet = struct.unpack('!IIII', ack)
                 if left_window == seq:
-                    mychecksum = zlib.crc32(seq,length,ty)
-                    if (checksum == mychecksum):
+                    checksum_calculated = zlib.crc32(seq,length,ty)
+                    if (checksum_in_packet == checksum_calculated):
                         left_window += 1 # ****left needs to maintain window size diff with right
                         right_window = min(right_window + 1, len(self.packet)-1)
                         curr += 1
                         self.received_pkts = None
+                        self.lock.acquire()
+                        self.fLog.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}""\n")
+                        self.lock.release()
                         return left_window, right_window, curr
-                    else:
-                        pass # ignore corrupt files
+                    else: # ignore corrupt files
+                        self.lock.acquire()
+                        self.fLog.write(f"Packet received; type={ty}; seqNum={seq}; length={length}; checksum_in_packet={checksum_in_packet}; checksum_calculated={checksum_calculated}""\n")
+                        self.lock.release()
                 else: # check for triple acks
                     if seq in self.seen_acks:
                         self.seen_acks[seq] += 1
                         if self.seq_acks[seq] >= 3:
-                            self.retransmit_oldest_packet()
+                            self.lock.acquire()
+                            self.fLog.write("Triple dup acks received for packet seqNum=%d", seq)
+                            self.lock.release()
                     else:
                         self.seen_acks[seq] = 1
+        else:
+            self.lock.acquire()
+            self.fLog.write("Timeout for packet seqNum=%d",seq)
+            self.lock.release()
+
+        self.retransmit_oldest_packet()
         curr = left_window
         left_window = 0
         right_window = 0
@@ -78,10 +101,6 @@ class PacketSender:
     def main(self):
 
         packet = processor.create_packet()
-        
-        # opening log file to start logging
-        fLog = open(log_file,'a')
-        fLog.write("Starting\n")
 
         # open client socket and bind
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -101,19 +120,13 @@ class PacketSender:
                 while curr_pkt <= right_window:
                     unreliable_channel.send_packet(UDPClientSocket, packet[curr_pkt], ip_address) # add receiver addy
                     self.lock.aquire()
-                    # log info to fLog
+                    ty, seq, length, checksum = struct.unpack('!IIII',  packet[curr_pkt])
+                    self.fLog.write(f"Packet sent; type={ty}; seqNum={seq}; length={length}; checksum={checksum}""\n")
                     self.lock.release()
                     curr_pkt += 1
+
                 left, right, curr = self.extract_packet(left_window, right_window, curr_pkt)
-            
-            if left == 0 & right == 0 & curr != 0: # no packets received due to timeout so resend
-                start_time = time.time()
-                while time.time() - start_time < 500/1000:
-                    unreliable_channel.send_packet(UDPClientSocket, packet[curr], ip_address) # add receiver addy
-                    left, right, curr = self.extract_packet(left_window, right_window, curr_pkt)
-
-
-                    
+   
             #update window,left and right
             left_window = left
             right_window = right
